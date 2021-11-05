868cb974856f60a174e488a4094aa73e7829fee3
Wastik Ansari <mdwastikansari521@gmail.com>
2021-11-05 20:17:57 +0530

    First commit

---
+++ android/app/src/main/kotlin/com/example/flutter_master_course/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_master_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child:  Text(
+        'Hello Flutter',
+        textDirection: TextDirection.ltr,git
+        style: TextStyle(fontSize: 40.0),
+      ),
+    )
+  );
+}
