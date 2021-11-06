e961aed27cb50c79305b3a7c60b4c471a30253ae
Wastik Ansari <mdwastikansari521@gmail.com>
2021-11-06 21:51:15 +0530

    Use Row and Column to Present Person Details

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -5,22 +5,70 @@ class homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        alignment: Alignment.center,
-        color: Colors.green,
-        height: 150.0,
-        width: 350.0,
-        margin: EdgeInsets.only(right: 30.0, bottom: 400.0, left: 30.0),
-        child: Text("Application",
-        style: TextStyle(
-          decoration: TextDecoration.none,
-          fontFamily: 'Roboto',
-          fontWeight: FontWeight.bold,
-          fontStyle: FontStyle.italic,
-          color: Colors.black
+        padding: EdgeInsets.only(left: 20.0,top: 55.0, right: 20.0),
+          alignment: Alignment.center,
+          color: Colors.amberAccent,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      "Name",
+                      style: TextStyle(
+                        fontSize: 30.0,
+                          decoration: TextDecoration.none,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.black),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      "Raju Rastogi",
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontFamily: 'Roboto',
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.black),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      "Adress",
+                      style: TextStyle(
+                          fontSize: 30.0,
+                          decoration: TextDecoration.none,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.black),
+                    ),
+                  ),
 
-        ),
-        ),
-      ),
+                  Expanded(
+                    child: Text(
+                      "Kanpur",
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontFamily: 'Roboto',
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.black),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          )),
     );
   }
 }
